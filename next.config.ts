3da041826e983636b2340288720f754c887b2c79
Nguyen Thanh Quang <quangnguyen31072004@gmail.com>
2026-04-01 13:28:27 +0700

    feature/shop-ui

--- next.config.ts
+++ next.config.ts
@@ -8,7 +8,7 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "res.cloudinary.com",
         port: "",
-        pathname: "/**",
+        pathname: "/**", 
       },
       {
         protocol: "https",
